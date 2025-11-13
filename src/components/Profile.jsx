3f8b1faeebc5051a6bb11bed12134dadccd92759
ankurbadhwania <ankurbadhwania@gmail.com>
2025-11-13 23:26:39 +0530

    bug fixes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,31 +8,7 @@ const Profile = () => {
     <>
     {user && <EditProfile user = {user}/>}
     </>
-    // <div className="flex justify-center my-10">
-    //   <div className="fieldset bg-base-200 border-base-300 rounded-box w-xs  border p-4">
-    //     <label className="label">Email </label>
-    //     <input
-    //       type="text"
-    //       className="input"
-    //       placeholder="Email"
-    //       // value={emailId}
-    //       // onChange={(e) => setEmailId(e.target.value)}
-    //     />
-
-    //     <label className="label">Password</label>
-    //     <input
-    //       type="text"
-    //       className="input"
-    //       placeholder="Password"
-    //       // value={password}
-    //       // onChange={(e) => setPassword(e.target.value)}
-    //     />
-    //     {/* <p className="text-red-500 font-semibold">{error}</p>   */}
-    //     <button className="btn btn-neutral mt-4" >
-    //       Login
-    //     </button>
-    //   </div>
-    // </div>
+   
   )
 }
 
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -49,15 +49,6 @@ const UserCard = ({ user }) => {
               Interested
             </button>
           </div>
-
-          {/* <div className="card-actions justify-center my-4">
-        <button className="btn btn-secondary">
-          Ignore
-        </button>
-        <button className="btn btn-primary">
-          Interested
-        </button>
-      </div> */}
         </div>
       </div>
     </div>
@@ -68,42 +59,3 @@ export default UserCard;
 
 
 
-
-
-
-/////////////////////////////////////////////////////////////////
-
-// const UserCard = ({user}) => {
-//     const {firstName, lastName, photoUrl, about, gender, age} = user;
-//     console.log(user)
-//   return (
-
-//     <div className="flex justify-center my-10">
-//     <div className="card w-80 bg-base-200 shadow-xl">
-//       <figure className="relative overflow-hidden">
-//         <img
-//           src={photoUrl}
-//           alt="userPhoto"
-//           className="w-full h-40 object-cover"
-//         />
-//         <div className="absolute inset-0 bg-base-100/20 opacity-0 hover:opacity-100 transition duration-300"></div>
-//       </figure>
-//       <div className="card-body">
-//         <h2 className="card-title">{firstName + " " + lastName}</h2>
-//         {age && gender && <p>{age + ", " + gender}</p>}
-//         <p>{about}</p>
-//         <div className="card-actions justify-center my-4">
-//           <button className="px-4 py-2 rounded-lg font-semibold text-[16px]
-// text-white bg-red-500 hover:bg-red-600 active:bg-red-700
-// shadow-md hover:shadow-lg transition">Ignore</button>
-
-//           <button className="px-4 py-2 rounded-lg font-semibold text-[16px]
-// text-white bg-blue-500 hover:bg-blue-600 active:bg-blue-700
-// shadow-md hover:shadow-lg transition">Interested</button>
-//         </div>
-//       </div>
-//     </div>
-//     </div>
-//   );
-// }
-// export default UserCard;
